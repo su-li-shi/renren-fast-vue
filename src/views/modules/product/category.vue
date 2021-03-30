4878eb704d0109ebb02cff7d4d6badd8ccf140c9
任文浩 <8506326+ren-wenhao@user.noreply.gitee.com>
2021-03-30 18:13:35 +0800

    3.30

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+      >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => edit(data)">
+            edit
+          </el-button>
+          <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      >
+      <el-form :model="category">
+        <el-form-item label="分类名称" >
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+      title: "",
+      dialogType: "", // add/edit
+      category: {
+        name: '',
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null
+      },
+      dialogVisible: false,
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  created () {
+    this.getMenus()
+  },
+  methods: {
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log('成功获取到菜单数据...', data.data)
+        this.menus = data.data
+      });
+    },
+    append(data){
+      console.log("append",data);
+      this.dialogType = "add"
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel*1 + 1;
+    },
+    // 添加三级分类方法
+    addCategory(){
+      console.log("提交的三级分类数据",this.category);
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单保存成功",
+          type: 'success'
+        })
+        this.dialogVisible=false;
+        this.getMenus();
+        //设置需要默认展开的菜单
+        this.expandedKey = [this.category.parentCid]
+      });
+    },
+    edit(data){
+      console.log("要修改的数据",data)
+      this.dialogType = "edit"
+      this.dialogVisible = true
+      this.category.name = data.name
+      this.category.catId = data.catId
+    },
+    remove(node,data){
+      var ids = [data.catId];
+      this.$confirm(`是否删除【${data.name}】菜单？`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({ data }) => {
+          this.$message({
+            message: "菜单删除成功",
+            type: "success"
+          });
+          //刷新出新的菜单
+          this.getMenus();
+          //设置需要默认展开的菜单
+          this.expandedKey = [node.parent.data.catId]
+        });
+      }).catch(() =>{});
+      console.log("remove",node,data)
+    },
+    submitData(){
+      if(this.dialogType == "add"){
+        this.addCategory();
+      }
+      if(this.dialogType == "edit"){
+        this.editCategory()
+      }
+    },
+    editCategory(){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
